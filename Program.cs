ad7e20b1b767d31377e795008e10ea8939caf9fd
Mubeena <72301900+mubeena123456@users.noreply.github.com>
2022-08-20 05:16:35 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("The current day is " + DateTime.Now.ToString("dddd"));
+Console.WriteLine("The current month is " + DateTime.Now.ToString("MMMM"));
+Console.WriteLine("The current time is " + DateTime.Now);
+
